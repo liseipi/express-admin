fb160001b83938a1241e2c28318d104ee1c0c273
liseipi <oli@qq.com>
2020-11-03 14:58:26 +0800

    add migration

--- src/database/schema/profile.ts
+++ src/database/schema/profile.ts
@@ -1,13 +1,13 @@
 export default async (connection: any) => {
-  let tabName = 'admin'
+  let tabName = 'profile'
 
   const dropSql = `DROP TABLE IF EXISTS ${tabName};`
   await connection.promise().query(dropSql)
 
   const statement = `CREATE TABLE IF NOT EXISTS ${tabName} (
     id                  INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email               CHAR(255) NOT NULL UNIQUE KEY,
-    passwrod            CHAR(255) NOT NULL,
+    pid                 INT NOT NULL UNIQUE KEY,
+    username            CHAR(255),
     createdAt           BIGINT NOT NULL,
     updatedAt           BIGINT NOT NULL
   ) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;`
